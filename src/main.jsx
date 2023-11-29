4cca12fee7ac13784b9d3d310a86718181ec6094
Hernan Casasola <67485423+GuidoMaxier@users.noreply.github.com>
2023-11-29 18:32:01 -0300

    Elliminamos comentarios
    - se elimino los comentarios en la linea 11 y 15 main.js

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ const root = document.getElementById('root');
 createRoot(root).render(
   <>
     <div>
-      {/* Mostrar el componente Reloj en un div separado */}
+      
       <Reloj />
     </div>
     <div>
-      {/* Mostrar la lista de tareas (componente App) en otro div */}
+      
       <h1 className='titleNeon'>LISTA DE TAREAS</h1>
       <App />
     </div>
