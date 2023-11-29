c90aaaf498a37dff6476aa7688f7a961a820b3b5
Sinches-Hugo-Ivan <hugo.ivan.sinches@gmail.com>
2023-11-29 18:36:10 -0300

    se da formato

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,17 @@
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-import { Reloj } from './components/Reloj';
-import './index.css';
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import { Reloj } from "./components/Reloj";
+import "./index.css";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 createRoot(root).render(
   <>
     <div>
-      
       <Reloj />
     </div>
     <div>
-      
-      <h1 className='titleNeon'>LISTA DE TAREAS</h1>
+      <h1 className="titleNeon">LISTA DE TAREAS</h1>
       <App />
     </div>
   </>
